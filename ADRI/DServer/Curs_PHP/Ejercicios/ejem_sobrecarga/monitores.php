9fb6e672a87b267db734ac1efa5e3f3f8c6e3e9d
FenFex <adrmar3@alu.edu.gva.es>
2024-10-29 12:58:16 +0100

    cambio

--- ADRI/DServer/Curs_PHP/Ejercicios/ejem_sobrecarga/monitores.php
+++ ADRI/DServer/Curs_PHP/Ejercicios/ejem_sobrecarga/monitores.php
@@ -1,14 +1,20 @@
 <?php
-
+include("modificar_ficheros/fichero.php");
 class monitores{
 
     private $nombre;
-
+    private $fichmonitor = "ficheros/monitores.txt";
+    private $fichero;
     public function __construct($nom){
         $this->nombre=$nom;
+        $this->fichero = new fichero($this->fichmonitor);
     }
 
     public function verMonitor(){
         echo $this->nombre;
+        $contenido = $this->fichero->leer();
+        for ($i=0; $i <count($contenido) ; $i++) { 
+            echo $contenido;
+        }
     }
 }
