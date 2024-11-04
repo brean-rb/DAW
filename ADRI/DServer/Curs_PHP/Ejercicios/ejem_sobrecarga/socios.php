1463b2a818b1020899691991ee567ab8ee4e8fd4
FenFex <FenFex@alu.com>
2024-11-04 18:12:21 +0100

    cambios

--- ADRI/DServer/Curs_PHP/Ejercicios/ejem_sobrecarga/socios.php
+++ ADRI/DServer/Curs_PHP/Ejercicios/ejem_sobrecarga/socios.php
@@ -1,36 +1,4 @@
 <?php
 include("modificar_ficheros/fichero.php");
-class socios{
 
-    private $fichero;
-    public function __construct(){
-        $this->fichero = new fichero("modificar_ficheros/ficheros/socios.txt");
-    }
-
-    public function verSocio($nom){
-       
-    }
-
-    public function anadirSocio($nom){
-        $grabado = $this->fichero->nuevo($nom);
-        $dev = false;
-        if ($grabado) {
-            $contenido = $this->fichero->leer();
-            for ($i=0; $i <count($contenido) ; $i++) { 
-                if ($nom === $contenido[$i]) {
-                    $dev = true;
-                }
-            } 
-        }
-        if ($dev) {return $contenido;}
-    }
-
-    public function eliminarSocio($nom){
-        $eliminado =$this->fichero->eliminar($nom);
-        $borrado = false;
-        if ($eliminado) {
-            $borrado = true;
-        }
-        return $borrado;
-    }
-}
+?>
