7904c1c8443db26bc8a116bc472fd3395976cc0f
ff <usuari@domini.com>
2025-01-16 19:31:17 +0100

    dia 16

---
+++ ADRI/DCliente/EJERCICIOS/JQUERY/carrito/scripts/carro.js
@@ -0,0 +1,11 @@
+$(function()
+{
+    $(".item").css("background-color","#cecece");
+    $("#cart_items").css("border","4px solid black");
+    $("img").css("border","blue 1px solid");
+    $(".item >  label").css("text-decoration", "underline");
+    $("#cart_container button").css("color","red");
+    $(".item label+label").css("color","white");       
+    $('[id*=€] input').css('color', 'green');
+    
+    });
