8617436ec8df34a015d8a1ece5bee57c5e8efb8b
brean-rb <rubenfm2004@gmail.com>
2025-01-17 15:42:30 +0100

    15-16/01/25

---
+++ RUBEN/DESAROLLO_WEB_CLIENTE/jQuery/tema 1/Ejercicio 1 JQuery-20250116/carrito/scripts/carro.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.item').css('background-color', '#cecece');
+    $('#cart_items').css('border', '4px solid black');
+    $('img').css('border', '1px solid blue');
+    $('.item > label').css('text-decoration', 'underline');
+    $('#cart_container button').css('color', 'red');
+    $('.item label+label').css('color', 'white');
+});
