252d65c14528db4d23ae5966fe2c3d3520b602e8
brean-rb <rubenfm2004@gmail.com>
2025-01-17 20:13:12 +0100

    17/01/25

--- RUBEN/DESAROLLO_WEB_CLIENTE/jQuery/tema 1/Ejercicio 1 JQuery-20250116/carrito/scripts/carro.js
+++ RUBEN/DESAROLLO_WEB_CLIENTE/jQuery/tema 1/Ejercicio 1 JQuery-20250116/carrito/scripts/carro.js
@@ -5,4 +5,8 @@ $(function() {
     $('.item > label').css('text-decoration', 'underline');
     $('#cart_container button').css('color', 'red');
     $('.item label+label').css('color', 'white');
+    $(':contains("€"), input').css('color', 'green');
+    $('div:empty').css('background-color', 'yellow');
+    $('.item:first, .item:last').css('background-color', 'red');
+    $('img[src*="camiseta"]').css('border', '2px solid green');
 });
