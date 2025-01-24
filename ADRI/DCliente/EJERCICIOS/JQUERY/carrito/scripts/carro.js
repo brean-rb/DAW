dc55531ecfb38df7da0d9ed80cae10579b9dc94f
FenFex <FenFex@>
2025-01-24 16:19:17 +0100

    cambiso

--- ADRI/DCliente/EJERCICIOS/JQUERY/carrito/scripts/carro.js
+++ ADRI/DCliente/EJERCICIOS/JQUERY/carrito/scripts/carro.js
@@ -1,13 +1,4 @@
 $(function()
 {
-    $(".item").css("background-color","#cecece");
-    $("#cart_items").css("border","4px solid black");
-    $("img").css("border","blue 1px solid");
-    $(".item >  label").css("text-decoration", "underline");
-    $("#cart_container button").css("color","red");
-    $(".item label+label").css("color","white");       
-    $('*:contains("€"),input').css('color', 'green'); 
-    $('div:empty').css("background-color","yellow");
-    $('.item:first,.item:last').css("background-color","red");
-    $('img[src*="camiseta"]').css("border", "2px solid green");
+   
 });
